02ac6523e1a99c0a0fb95eb55d3e97f18343bdad
Harsh Singh Rathore <thecreator@pop-os.localdomain>
2022-11-14 18:14:12 +0530

    made read write func

--- main.go
+++ main.go
@@ -1,34 +1,62 @@
 package main
 
 import (
-	// "bufio"
+	"bufio"
 	// "context"
 	"crypto/rand"
 	// "crypto/sha256"
 	// "encoding/hex"
-	// "encoding/json"
+	"encoding/json"
 	// "flag"
 	"fmt"
 	"io"
 	"log"
 	mrand "math/rand"
-	// "os"
+	"os"
 	// "strconv"
-	// "strings"
-	// "sync"
-	// "time"
+	"strings"
+	"sync"
+	"time"
 
-	// "github.com/davecgh/go-spew/spew"
+	"github.com/davecgh/go-spew/spew"
 	// golog "github.com/ipfs/go-log"
 	libp2p "github.com/libp2p/go-libp2p"
 	crypto "github.com/libp2p/go-libp2p-core/crypto"
 	host "github.com/libp2p/go-libp2p-core/host"
-	// net "github.com/libp2p/go-libp2p-net"
+	net "github.com/libp2p/go-libp2p-core/network"
+
 	// peer "github.com/libp2p/go-libp2p-peer"
 	// pstore "github.com/libp2p/go-libp2p-peerstore"
 	ma "github.com/multiformats/go-multiaddr"
 	// gologging "github.com/whyrusleeping/go-logging"
 )
+
+type Block struct {
+	Index int
+	Value string
+}
+
+// Blockchain is a series of validated Blocks
+var Blockchain []Block
+
+var mutex = &sync.Mutex{}
+
+func isBlockValid(newBlock, oldBlock Block) bool {
+	if oldBlock.Index+1 != newBlock.Index {
+		return false
+	}
+	return true
+}
+
+func generateBlock(oldBlock Block, value string) Block {
+
+	var newBlock Block
+
+	newBlock.Index = oldBlock.Index + 1
+	newBlock.Value = value
+
+	return newBlock
+}
 
 // makeBasicHost creates a LibP2P host with a random peer ID listening on the
 // given multiaddress. It will use secio if secio is true.
@@ -80,6 +108,109 @@ func makeBasicHost(listenPort int, secio bool, randseed int64) (host.Host, error
 	}
 
 	return basicHost, nil
+}
+func readData(rw *bufio.ReadWriter) {
+
+	for {
+		str, err := rw.ReadString('\n')
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if str == "" {
+			return
+		}
+		if str != "\n" {
+
+			chain := make([]Block, 0)
+			if err := json.Unmarshal([]byte(str), &chain); err != nil {
+				log.Fatal(err)
+			}
+
+			mutex.Lock()
+			if len(chain) > len(Blockchain) {
+				Blockchain = chain
+				bytes, err := json.MarshalIndent(Blockchain, "", "  ")
+				if err != nil {
+
+					log.Fatal(err)
+				}
+				// Green console color: 	\x1b[32m
+				// Reset console color: 	\x1b[0m
+				fmt.Printf("\x1b[32m%s\x1b[0m> ", string(bytes))
+			}
+			mutex.Unlock()
+		}
+	}
+}
+func writeData(rw *bufio.ReadWriter) {
+
+	go func() {
+		for {
+			time.Sleep(5 * time.Second)
+			mutex.Lock()
+			bytes, err := json.Marshal(Blockchain)
+			if err != nil {
+				log.Println(err)
+			}
+			mutex.Unlock()
+
+			mutex.Lock()
+			rw.WriteString(fmt.Sprintf("%s\n", string(bytes)))
+			rw.Flush()
+			mutex.Unlock()
+
+		}
+	}()
+
+	stdReader := bufio.NewReader(os.Stdin)
+
+	for {
+		fmt.Print("> ")
+		sendData, err := stdReader.ReadString('\n')
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		sendData = strings.Replace(sendData, "\n", "", -1)
+		// value, err := strconv.Atoi(sendData)
+		// if err != nil {
+		// 	log.Fatal(err)
+		// }
+		newBlock := generateBlock(Blockchain[len(Blockchain)-1], sendData)
+
+		if isBlockValid(newBlock, Blockchain[len(Blockchain)-1]) {
+			mutex.Lock()
+			Blockchain = append(Blockchain, newBlock)
+			mutex.Unlock()
+		}
+
+		bytes, err := json.Marshal(Blockchain)
+		if err != nil {
+			log.Println(err)
+		}
+
+		spew.Dump(Blockchain)
+
+		mutex.Lock()
+		rw.WriteString(fmt.Sprintf("%s\n", string(bytes)))
+		rw.Flush()
+		mutex.Unlock()
+	}
+
+}
+
+func handleStream(s net.Stream) {
+
+	log.Println("Got a new stream!")
+
+	// Create a buffer stream for non blocking read and write.
+	rw := bufio.NewReadWriter(bufio.NewReader(s), bufio.NewWriter(s))
+
+	go readData(rw)
+	go writeData(rw)
+
+	// stream 's' will stay open until you close it (or the other side closes it).
 }
 
 func main() {
