397e62b4a4b25ac2ee49ff365bd1a77ec0ec02d0
Harsh Singh Rathore <thecreator@pop-os.localdomain>
2022-11-15 11:20:49 +0530

    p2p network w replicating memory in all nodes

--- main.go
+++ main.go
@@ -2,31 +2,31 @@ package main
 
 import (
 	"bufio"
-	// "context"
+	"context"
 	"crypto/rand"
 	// "crypto/sha256"
 	// "encoding/hex"
 	"encoding/json"
-	// "flag"
+	"flag"
 	"fmt"
 	"io"
 	"log"
 	mrand "math/rand"
 	"os"
 	// "strconv"
-	"strings"
+	// "strings"
 	"sync"
 	"time"
 
 	"github.com/davecgh/go-spew/spew"
-	// golog "github.com/ipfs/go-log"
+	golog "github.com/ipfs/go-log"
 	libp2p "github.com/libp2p/go-libp2p"
 	crypto "github.com/libp2p/go-libp2p-core/crypto"
 	host "github.com/libp2p/go-libp2p-core/host"
 	net "github.com/libp2p/go-libp2p-core/network"
 
-	// peer "github.com/libp2p/go-libp2p-peer"
-	// pstore "github.com/libp2p/go-libp2p-peerstore"
+	peer "github.com/libp2p/go-libp2p/core/peer"        //"github.com/libp2p/go-libp2p-core/peer"
+	pstore "github.com/libp2p/go-libp2p/core/peerstore" //"github.com/libp2p/go-libp2p/p2p/host/peerstore"
 	ma "github.com/multiformats/go-multiaddr"
 	// gologging "github.com/whyrusleeping/go-logging"
 )
@@ -172,11 +172,6 @@ func writeData(rw *bufio.ReadWriter) {
 			log.Fatal(err)
 		}
 
-		sendData = strings.Replace(sendData, "\n", "", -1)
-		// value, err := strconv.Atoi(sendData)
-		// if err != nil {
-		// 	log.Fatal(err)
-		// }
 		newBlock := generateBlock(Blockchain[len(Blockchain)-1], sendData)
 
 		if isBlockValid(newBlock, Blockchain[len(Blockchain)-1]) {
@@ -214,10 +209,88 @@ func handleStream(s net.Stream) {
 }
 
 func main() {
-	// new host.Host
-	ha, err := makeBasicHost(0, true, 0)
+	// t := time.Now()
+	genesisBlock := Block{}
+	genesisBlock = Block{0, ""}
+
+	Blockchain = append(Blockchain, genesisBlock)
+
+	// LibP2P code uses golog to log messages. They log with different
+	// string IDs (i.e. "swarm"). We can control the verbosity level for
+	// all loggers with:
+	golog.SetAllLoggers(golog.LevelInfo) // to get logging info
+
+	// Parse options from the command line
+	listenF := flag.Int("l", 0, "wait for incoming connections")
+	target := flag.String("d", "", "target peer to dial")
+	secio := flag.Bool("secio", false, "enable secio")
+	seed := flag.Int64("seed", 0, "set random seed for id generation")
+	flag.Parse()
+
+	if *listenF == 0 {
+		log.Fatal("Please provide a port to bind on with -l")
+	}
+
+	// Make a host that listens on the given multiaddress
+	ha, err := makeBasicHost(*listenF, *secio, *seed)
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Print(ha)
+
+	if *target == "" {
+		log.Println("listening for connections")
+		// Set a stream handler on host A. /p2p/1.0.0 is
+		// a user-defined protocol name.
+		ha.SetStreamHandler("/p2p/1.0.0", handleStream)
+
+		select {} // hang forever
+		/**** This is where the listener code ends ****/
+	} else {
+		ha.SetStreamHandler("/p2p/1.0.0", handleStream)
+
+		// The following code extracts target's peer ID from the
+		// given multiaddress
+		ipfsaddr, err := ma.NewMultiaddr(*target)
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		pid, err := ipfsaddr.ValueForProtocol(ma.P_IPFS)
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		peerid, err := peer.Decode(pid)
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		// Decapsulate the /ipfs/<peerID> part from the target
+		// /ip4/<a.b.c.d>/ipfs/<peer> becomes /ip4/<a.b.c.d>
+		targetPeerAddr, _ := ma.NewMultiaddr(
+			fmt.Sprintf("/ipfs/%s", peer.Encode(peerid)))
+		targetAddr := ipfsaddr.Decapsulate(targetPeerAddr)
+
+		// We have a peer ID and a targetAddr so we add it to the peerstore
+		// so LibP2P knows how to contact it
+		ha.Peerstore().AddAddr(peerid, targetAddr, pstore.PermanentAddrTTL)
+
+		log.Println("opening stream")
+		// make a new stream from host B to host A
+		// it should be handled on host A by the handler we set above because
+		// we use the same /p2p/1.0.0 protocol
+		s, err := ha.NewStream(context.Background(), peerid, "/p2p/1.0.0")
+		if err != nil {
+			log.Fatalln(err)
+		}
+		// Create a buffered stream so that read and writes are non blocking.
+		rw := bufio.NewReadWriter(bufio.NewReader(s), bufio.NewWriter(s))
+
+		// Create a thread to read and write data.
+		go writeData(rw)
+		go readData(rw)
+
+		select {} // hang forever
+
+	}
 }
